4a5aa39254586940db780281a7477ffaf9351238
anna200167 <hopperdevil200167@gmail.com>
2022-12-07 12:27:11 +0530

    Adding my new feature

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const rs = fs.createReadStream('lorem.txt',{encoding:'utf-8'});
 const ws = fs.createWriteStream('hello.txt');
 
 
+try{
+
 
 rs.on('data',(data)=>{
     console.log(data)
@@ -29,3 +31,6 @@ rs.error('data',(data)=>{
     console.log(data)
     console.log(data)
 })
+}catch(error){
+    console.log(error.messge);
+}
